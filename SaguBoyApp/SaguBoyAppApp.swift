1d3b5c69746f65cf7294f11fe3a078a5994bb3d9
vicenzorm <vimasera11@gmail.com>
2025-09-09 16:40:00 -0300

    restart

---
+++ SaguBoyApp/SaguBoyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaguBoyAppApp.swift
+//  SaguBoyApp
+//
+//  Created by Vicenzo Másera on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SaguBoyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
