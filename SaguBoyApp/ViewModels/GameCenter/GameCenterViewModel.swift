a167a1285b220daeae231098ce9d017c764b41c0
vicenzorm <vimasera11@gmail.com>
2025-09-12 14:25:53 -0300

    added gamecenter viewmodel

---
+++ SaguBoyApp/ViewModels/GameCenter/GameCenterViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  GameCenterViewModel.swift
+//  SaguBoyApp
+//
+//  Created by Vicenzo Másera on 12/09/25.
+//
+import GameKit
+import Foundation
+
+@MainActor
+class GameCenterViewModel: GameCenterViewModelProtocol {
+    
+    var isAuthReady: Bool = false
+    var player: GKLocalPlayer?
+    var showAuthSheet: Bool = false
+    
+    private var localPlayer: GKLocalPlayer {
+        return GKLocalPlayer.local
+    }
+    
+    init() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(playerAuthDidChange),
+            name: .GKPlayerAuthenticationDidChangeNotificationName,
+            object: nil
+        )
+    }
+    
+    func authPlayer() {
+        localPlayer.authenticateHandler = { [weak self] viewController, error in
+            guard let self = self else { return }
+            
+            if let vc = viewController {
+                self.showAuthSheet = true
+                return
+            }
+            
+            if let error {
+                print("deu erro na autenticação... \(error.localizedDescription)" )
+                self.isAuthReady = false
+                return
+            }
+            
+            if self.localPlayer.isAuthenticated {
+                self.isAuthReady = true
+                self.player = self.localPlayer
+            } else {
+                self.isAuthReady = false
+            }
+        }
+    }
+    
+    @objc func playerAuthDidChange() {
+        self.isAuthReady = self.localPlayer.isAuthenticated
+        self.player = localPlayer.isAuthenticated ? localPlayer : nil
+    }
+}
---
+++ SaguBoyApp/ViewModels/GameCenter/GameCenterViewModelProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  GameCenterViewModelProtocol.swift
+//  SaguBoyApp
+//
+//  Created by Vicenzo Másera on 12/09/25.
+//
+
+import Foundation
+
+
+protocol GameCenterViewModelProtocol {
+    
+}
